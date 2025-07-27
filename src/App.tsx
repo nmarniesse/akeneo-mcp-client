df7234405786975b8fbf9700cf2287b99175ee68
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2025-07-27 15:21:36 +0200

    Improve UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useChat } from './hooks/useChat';
 import { AdjustableTextarea } from './components/AdjustableTextarea';
 
 function App() {
   const [input, setInput] = React.useState<string>('');
   const { messages, isLoading, submit } = useChat();
+  const inputRef = React.useRef<HTMLInputElement>(null);
 
   const handleSubmit = (form: React.FormEvent<HTMLFormElement>) => {
     form.preventDefault();
     submit(input);
     setInput('');
   };
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [messages]);
   
   return (
     <>
@@ -22,15 +29,19 @@ function App() {
               <td><AdjustableTextarea text={message.content} /></td>
             </tr>
           ))}
+          <tr>
+            <td><strong>Input</strong></td>
+            <td>
+              <form onSubmit={handleSubmit}>
+                <input ref={inputRef} style={{width: '600px'}} name="prompt" value={input} onChange={(event) => setInput(event.target.value)} disabled={isLoading} />
+                <button type="submit">Submit</button>
+              </form>
+            </td>
+          </tr>
         </tbody>
       </table>
 
       {isLoading && <div>Loading...</div>}
-      <br/><br/>
-      <form onSubmit={handleSubmit}>
-        <input style={{width: '800px'}} name="prompt" value={input} onChange={(event) => setInput(event.target.value)} disabled={isLoading} />
-        <button type="submit">Submit</button>
-      </form>
     </>
   );
 }
